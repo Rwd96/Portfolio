a2e69d8c8a4b6bcb86abf567fe3bec75cbf8bde2
Or Sade <or@connectcom.co.il>
2024-02-13 15:17:50 +0200

    test3

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "www.orsade.click-portfolio-terraform.tfstate"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.33.0"
+      version = "5.33.0"    
     }
   }
 }
@@ -10,3 +16,4 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
