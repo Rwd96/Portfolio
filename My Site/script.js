886722ca5889e0f6e84c3916bc7cad4154eb762f
Or Sade <or@connectcom.co.il>
2024-02-14 14:21:31 +0200

    test

--- My Site/script.js
+++ My Site/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Network Specialist', 'Cloud Architect'],
+    strings: ['Network Specialist'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
